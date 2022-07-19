cc78f2cdc84bcb19cfc25c23e908af412f4a2074
granking <grankingster@gmail.com>
2022-07-19 16:06:41 -0500

    a medias el trycatch

--- src/async/challenge.js
+++ src/async/challenge.js
@@ -1,3 +1,16 @@
 import fetch from 'node-fetch';
 const API = 'https://api.escuelajs.co/api/v1';
 
+async function fetchData(urlApi) {
+    const response = await fetch(urlApi);
+    const data = await response.json();
+    return data;
+}
+
+const anotherFunction = async (urlApi) => {
+    try {
+        
+    } catch () {
+        
+    }
+}
